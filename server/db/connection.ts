b7407808fbd449a839ceb54e89cc1e1946be9a1c
student <student@pop-os.localdomain>
2023-03-31 09:40:09 +1300

    big reub

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,10 +1,15 @@
 import knexFile from './knexfile'
 import knex from 'knex'
+import { 'meme' } from 
 
 type Environment = 'production' | 'test' | 'development'
 
 const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 const connection = knex(config)
+
+export function getAllMemes(db = connection): promise<'Meme'[]> {
+  return db('memes').select()
+}
 
 export default connection
