4a0fd07c9a11ac690f3c22ba552360201284ddb4
student <student@pop-os.localdomain>
2023-03-31 12:05:05 +1300

    what the actual

--- client/components/MemeForm.tsx
+++ client/components/MemeForm.tsx
@@ -1,6 +1,7 @@
-import { useState, FormEvent, ChangeEvent } from 'react'
+import { useEffect, useState, FormEvent, ChangeEvent } from 'react'
 import { receiveForm } from '../actions/meme'
-import { useAppDispatch } from '../hooks/redux'
+import { useAppDispatch, useAppSelector } from '../hooks/redux'
+import { fetchMeme } from '../actions/meme'
 
 export default function MemeForm() {
   const [formData, setFormData] = useState({ toptext: '', bottomtext: '' })
@@ -8,7 +9,6 @@ export default function MemeForm() {
 
   const submitHandler = (e: FormEvent) => {
     e.preventDefault()
-    console.log(formData)
 
     dispatch(receiveForm(formData.bottomtext, formData.toptext))
     // api call here
@@ -17,19 +17,31 @@ export default function MemeForm() {
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.id]: e.target.value })
   }
-  return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="toptext">TOP TEXT: </label>
-      <input type="text" id="toptext" name="toptext" onChange={changeHandler} />
 
-      <label htmlFor="toptext">BOTTOM TEXT: </label>
-      <input
-        type="text"
-        id="bottomtext"
-        name="bottomtext"
-        onChange={changeHandler}
-      />
-      <button onClick={submitHandler}>subbie</button>
-    </form>
+  useEffect(() => {
+    dispatch(fetchMeme())
+  }, [])
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="toptext">TOP TEXT: </label>
+        <input
+          type="text"
+          id="toptext"
+          name="toptext"
+          onChange={changeHandler}
+        />
+
+        <label htmlFor="toptext">BOTTOM TEXT: </label>
+        <input
+          type="text"
+          id="bottomtext"
+          name="bottomtext"
+          onChange={changeHandler}
+        />
+        <button onClick={submitHandler}>subbie</button>
+      </form>
+    </>
   )
 }
